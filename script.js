ca9dbbb86b48086d46400daa678332a5688f4a10
Emily Kanarek <emily.kanarek@powerhrg.com>
2024-02-20 15:57:38 -0500

    Set up cat translator button

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const container = document.querySelector(".container");
+const catButton = document.getElementById("translate-cat");
+const humanButton = document.getElementById("translate-human");
+
+catButton.addEventListener('click', function() {
+    const humanText = document.querySelector(".human");
+    const catText = document.querySelector(".meow");
+    humanText.style.display = "none";
+    catText.style.display = "block";
+    catButton.style.display = "none";
+    humanButton.style.display = "block";
+});
+
+humanButton.addEventListener('click', function() {
+    const humanText = document.querySelector(".human");
+    const catText = document.querySelector(".meow");
+    humanText.style.display = "block";
+    catText.style.display = "none";
+    catButton.style.display = "block";
+    humanButton.style.display = "none";
+});
+
+// button.addEventListener('click', function() {
+//     const humanText = document.querySelector(".human");
+//     const catText = document.querySelector(".meow");
+//     if (button.innerHTML = "Translate Into Cat") {
+//         humanText.style.display = "none";
+//         catText.style.display = "block";
+//         button.innerHTML = "Translate Into Human";
+//     } else {
+//         humanText.style.display = "block";
+//         catText.style.display = "none";
+//         button.innerHTML = "Translate Into Cat";
+//     }
+// })
+
+// function changeLanguages() {
+//     if (button.innerHTML = "Translate Into Cat") {
+//         button.addEventListener('click', function () {
+//             const humanText = document.querySelector(".human"); 
+//             humanText.style.display = "none";
+//             const catText = document.querySelector(".meow");
+//             catText.style.display = "block";
+//             button.innerHTML = "Translate to Human";
+//         });
+//     }
+//     else if (button.innerHTML = "Translate to Human") {
+//         button.addEventListener('click', function () {
+//             const humanText = document.querySelector(".human"); 
+//             humanText.style.display = "block";
+//             const catText = document.querySelector(".meow");
+//             catText.style.display = "none";
+//             button.innerHTML = "Translate to Cat";
+//     });
+    
+// }};

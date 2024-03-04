6ee8aa191dbdc4edfe2e38270c24c2566b28d3fa
Emily Kanarek <emily.kanarek@powerhrg.com>
2024-03-04 10:02:35 -0500

    Deleted old commented-out code

--- script.js
+++ script.js
@@ -19,38 +19,3 @@ humanButton.addEventListener('click', function() {
     catButton.style.display = "block";
     humanButton.style.display = "none";
 });
-
-// button.addEventListener('click', function() {
-//     const humanText = document.querySelector(".human");
-//     const catText = document.querySelector(".meow");
-//     if (button.innerHTML = "Translate Into Cat") {
-//         humanText.style.display = "none";
-//         catText.style.display = "block";
-//         button.innerHTML = "Translate Into Human";
-//     } else {
-//         humanText.style.display = "block";
-//         catText.style.display = "none";
-//         button.innerHTML = "Translate Into Cat";
-//     }
-// })
-
-// function changeLanguages() {
-//     if (button.innerHTML = "Translate Into Cat") {
-//         button.addEventListener('click', function () {
-//             const humanText = document.querySelector(".human"); 
-//             humanText.style.display = "none";
-//             const catText = document.querySelector(".meow");
-//             catText.style.display = "block";
-//             button.innerHTML = "Translate to Human";
-//         });
-//     }
-//     else if (button.innerHTML = "Translate to Human") {
-//         button.addEventListener('click', function () {
-//             const humanText = document.querySelector(".human"); 
-//             humanText.style.display = "block";
-//             const catText = document.querySelector(".meow");
-//             catText.style.display = "none";
-//             button.innerHTML = "Translate to Cat";
-//     });
-    
-// }};
